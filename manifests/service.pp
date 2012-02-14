ff35bb9ef5b01b4374f6e1a6eea415f7354e9bd3
nadir <nadir-technik@nadir.org>
2012-02-14 11:15:25 +0100

    working nrpe support

--- manifests/service.pp
+++ manifests/service.pp
@@ -21,7 +21,12 @@ define nagios::service (
     $real_name = "${hostname}_${name}"
 
     if ($use_nrpe == 'true') {
-        $real_check_command = "check_nrpe!$check_command!\"$nrpe_args\""
+        if ($nrpe_args != '') { 
+	    $real_check_command = "check_nrpe!$check_command!\"$nrpe_args\"" 
+	}
+	else { 
+	    $real_check_command = "check_nrpe_1arg!$check_command" 
+	}
     }
     else {
         $real_check_command = "$check_command"
@@ -30,7 +35,7 @@ define nagios::service (
 
     @@nagios_service { "${real_name}":
         ensure => $ensure,
-        check_command => $check_command,
+        check_command => $real_check_command,
         host_name => $host_name,
         use => $use,
         notify => Service[nagios],
