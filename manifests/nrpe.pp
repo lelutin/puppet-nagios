0b20e1e0fd001cb0b688c6bf6971c8e7c3f743c6
kwadronaut <kwadronaut@leap.se>
2013-01-25 15:18:11 +0100

    adding Debian specific nrpe template (fixes stop/restart of nrpe-server)

--- manifests/nrpe.pp
+++ manifests/nrpe.pp
@@ -8,6 +8,11 @@ class nagios::nrpe {
 
             include nagios::nrpe::freebsd
         }
+        'Debian': {
+            if $nagios_nrpe_pid_file == '' { $nagios_nrpe_pid_file = '/var/run/nagios/nrpe.pid' }
+            if $nagios_plugin_dir == '' { $nagios_plugin_dir = '/usr/lib/nagios/plugins' }
+            include nagios::nrpe::linux
+        }
         default: {
             if $nagios_nrpe_pid_file == '' { $nagios_nrpe_pid_file = '/var/run/nrpe.pid' }
             if $nagios_plugin_dir == '' { $nagios_plugin_dir = '/usr/lib/nagios/plugins' }
