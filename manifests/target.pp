4d4119d3c7b47ed34b7e014b3a9f9ff0bddda76c
mh <mh@immerda.ch>
2015-06-07 11:43:49 +0200

    cleanup things a bit and make it possible to set a different fqdn

--- manifests/target.pp
+++ manifests/target.pp
@@ -1,22 +1,26 @@
-# manifests/target.pp
-
+# a simple nagios target to monitor
 class nagios::target(
-  $parents = 'absent',
-  $address = $::ipaddress,
+  $parents      = 'absent',
+  $address      = $::ipaddress,
   $nagios_alias = $::hostname,
-  $hostgroups = 'absent'
+  $hostgroups   = 'absent',
+  $use          = 'generic-host',
 ){
   @@nagios_host { $::fqdn:
     address => $address,
-    alias => $nagios_alias,
-    use => 'generic-host',
+    alias   => $nagios_alias,
+    use     => $use,
   }
 
   if ($parents != 'absent') {
-    Nagios_host["${::fqdn}"] { parents => $parents }
+    Nagios_host[$::fqdn] {
+      parents => $parents
+    }
   }
 
   if ($hostgroups != 'absent') {
-    Nagios_host["${::fqdn}"] { hostgroups => $hostgroups }  
+    Nagios_host[$::fqdn] {
+      hostgroups => $hostgroups
+    }
   }
 }
