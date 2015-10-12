77419fa626578d14e55042a39b2b87e5c0904ae2
mh <mh@immerda.ch>
2015-10-13 00:57:06 +0200

    install requirements

--- manifests/plugins/horde_login.pp
+++ manifests/plugins/horde_login.pp
@@ -1,7 +1,9 @@
 # check_horde_login
 class nagios::plugins::horde_login {
+  ensure_packages(['python-requests'])
   nagios::plugin { 'check_horde_login':
-    source => 'nagios/plugins/check_horde_login',
+    source  => 'nagios/plugins/check_horde_login',
+    require => Package['python-requests'],
   } -> nagios_command {
     'check_horde_login':
       command_line => "\$USER1\$/check_horde_login -s \$ARG1\$ -u \$ARG2\$ -p \$ARG3\$",
